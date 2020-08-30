fe2f75ed90dff12335e3c5064b591111a911cc60
Lin Chen <57054774+Caleb411@users.noreply.github.com>
2020-08-30 22:53:10 +0800

    0080 added.

---
+++ 0080-Remove-Duplicates-from-Sorted-Array-II/Solution2.java
@@ -0,0 +1,25 @@
+/// Source : https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/
+/// Author : chenlin
+/// Time   : 2020-08-30
+/// Updated: 2020-08-30
+
+// Overwrite redundant duplicates
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        int count = 1;
+        for(int i = 1;i < nums.length;i++){
+            if(nums[i] == nums[i-1]){
+                count++;
+            }else{
+                count = 1;
+            }
+            if(count<=2){
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+}
