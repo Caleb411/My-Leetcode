0e4a15e4236ef180535e9491461bedaf18f14534
Lin Chen <57054774+Caleb411@users.noreply.github.com>
2020-09-04 12:30:19 +0800

    0215 solved.

---
+++ 0215-Kth-Largest-Element-in-an-Array/Solution.java
@@ -0,0 +1,58 @@
+/// Source : https://leetcode-cn.com/problems/kth-largest-element-in-an-array/
+/// Author : chenlin
+/// Time   : 2020-09-04
+/// Updated: 2020-09-04
+
+// sort2ways
+// Time Complexity: O(n)
+// Space Complexity: O(logn)
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Random rnd = new Random();
+        int n = nums.length;
+        sort(nums, 0, n-1, rnd, n-k);
+        return nums[n-k];
+    }
+
+    private void sort(int[] arr, int l, int r, Random rnd, int k){
+        if(l>=r) return;
+        int p = partition(arr, l, r, rnd);
+        if(p == k){
+            return;
+        }else if(p > k){
+            sort(arr, l, p-1, rnd, k);
+        }else{
+            sort(arr, p+1, r, rnd, k);
+        }
+    }
+
+    private int partition(int[] arr, int l, int r, Random rnd){
+        int p = l + rnd.nextInt(r-l+1);
+        swap(arr, l, p);
+
+        int i = l + 1;
+        int j = r;
+        while(true){
+            while(i<=j&&arr[i]<arr[l]){
+                i++;
+            }
+            while(j>=i&&arr[j]>arr[l]){
+                j--;
+            }
+
+            if(i >= j) break;
+
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+        swap(arr, l, j);
+        return j;
+    }
+
+    private void swap(int[] arr, int i, int j){
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+}
