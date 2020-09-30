376b56db546760c55dd625d10a569639d68fae11
Lin Chen <57054774+Caleb411@users.noreply.github.com>
2020-09-30 11:38:49 +0800

    0380 updated.

--- 0380-Insert-Delete-GetRandom-O(1)/Solution.java
+++ 0380-Insert-Delete-GetRandom-O(1)/Solution.java
@@ -3,13 +3,13 @@
 /// Time   : 2020-09-30
 /// Updated: 2020-09-30
 
-// 
+// random read
 // Time Complexity: O(1)
 // Space Complexity: O(n)
 class RandomizedSet {
 
-    List<Integer> list;
-    Map<Integer, Integer> map;
+    List<Integer> list; // 存储元素
+    Map<Integer, Integer> map;  // 存储元素到索引的映射
     Random random = new Random();
 
     /** Initialize your data structure here. */
@@ -19,6 +19,7 @@ class RandomizedSet {
     }
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    // 对数组尾部进行插入和删除操作不会涉及数据搬移，时间复杂度是 O(1)
     public boolean insert(int val) {
         if(map.containsKey(val)){
             return false;
@@ -29,6 +30,7 @@ class RandomizedSet {
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
+    // 如果我们想在O(1)的时间删除数组中的某一个元素val，可以先把这个元素交换到数组的尾部，然后再pop掉
     public boolean remove(int val) {
         if(!map.containsKey(val)){
             return false;
@@ -45,6 +47,7 @@ class RandomizedSet {
     }
     
     /** Get a random element from the set. */
+    // 要想随机读取，底层必须用数组实现，且数组必须是紧凑的
     public int getRandom() {
         return list.get(random.nextInt(list.size()));
     }
