ec3e4f5a992383d7ebe7ebaf8c2a56ba09bf5dcd
Chen Lin <57054774+Caleb411@users.noreply.github.com>
2020-10-20 12:39:50 +0800

    0739 solved.

---
+++ 0739-Daily-Temperatures/Solution.java
@@ -0,0 +1,23 @@
+/// Source : https://leetcode-cn.com/problems/daily-temperatures/
+/// Author : chenlin
+/// Time   : 2020-10-20
+/// Updated: 2020-10-20
+
+// monotonous stack
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// 这里计算的是距离，所以单调栈里存的是索引，比较的时候要根据索引取出值比较
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        int[] res = new int[T.length];
+        Deque<Integer> stack = new LinkedList<>();
+        for(int i = T.length-1;i >= 0;i--){
+            while(!stack.isEmpty() && T[stack.peek()] <= T[i]){
+                stack.pop();
+            }
+            res[i] = stack.isEmpty() ? 0: stack.peek()-i;
+            stack.push(i);
+        }
+        return res;
+    }
+}
